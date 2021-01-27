2730a6bd0ab8ec779ca5a862df81aaff35e4194b
Abimanyu Ananthu <abimanyuananthu@gmail.com>
2021-01-27 16:05:08 -0500

    init

--- src/Date.java
+++ src/Date.java
@@ -1,4 +1,20 @@
 import java.util.Calendar;
+import java.util.StringTokenizer;
 public class Date {
+    private int year;
+    private int month;
+    private int day;
 
+    //taking mm/dd/yyyy and create a Date object{ }
+    public Date(String date) {
+
+    }
+    //return today’s date
+    public Date() {
+
+    }
+
+    public boolean isValid() {
+
+    }
 }
