a827ba5435a59a5412459e5c77e770c126419aa8
ashenoy20 <ashishshenoy9@gmail.com>
2021-02-03 16:00:21 -0500

    JavaDoc for Book and Date classes

--- src/Date.java
+++ src/Date.java
@@ -1,6 +1,14 @@
 import java.util.Calendar;
 import java.util.StringTokenizer;
 
+/**
+ * A class that stores information about a single book.
+ * This information includes a serial number, the name of
+ * the book, check out status, and the date its been published.
+ *
+ * @author Abimanyu Ananthu, Ashish Shenoy
+ */
+
 public class Date {
     private int year;
     private int month;
@@ -10,24 +18,32 @@ public class Date {
     public static final int QUADRENNIAL = 4;
     public static final int CENTENNIAL = 100;
     public static final int QUATERCENTENNIAL = 400;
-    public static final int FEBRUARYLEAP = 29;
-    public static final int MONTHEND = 31;
-    public static final int OLDBOOK = 1900;
+    public static final int FEBRUARY_LEAP = 29;
+    public static final int MONTH_END = 31;
+    public static final int OLD_BOOK = 1900;
 
-    //taking mm/dd/yyyy and create a Date object {}
+    /**
+     * Creates a Date object based off the given string.
+     * Using StringTokenizer, it assigns the proper values
+     * to the instance variables month, day, and year
+     *
+     * @param date - a string in the form mm/dd/yyyy
+     */
+
     public Date(String date) {
-        int i = 0; //Keeps track of the iterations
         StringTokenizer st = new StringTokenizer(date, "/");
-        String[] arr = new String[3];
-        while(st.hasMoreTokens()){
-            arr[i] = st.nextToken();
-            i++;
-        }
-        this.month = Integer.parseInt(arr[0]);
-        this.day = Integer.parseInt(arr[1]);
-        this.year = Integer.parseInt(arr[2]);
+        this.month = Integer.parseInt(st.nextToken());
+        this.day = Integer.parseInt(st.nextToken());
+        this.year = Integer.parseInt(st.nextToken());
     }
-    //return today’s date
+
+    /**
+     * Creates a Date object that represents today's date
+     * with the help of the Java Calender library.
+     * In the event the user doesn't specify a string,
+     * this constructor will run
+     */
+
     public Date() {
         Calendar rightNow = Calendar.getInstance();
         this.year = rightNow.get(Calendar.YEAR);
@@ -35,27 +51,55 @@ public class Date {
         this.day = rightNow.get(Calendar.DAY_OF_MONTH);
     }
 
+    /**
+     * Returns the year from the date object
+     *
+     * @return integer value representing the year of the date
+     */
     public int getYear(){
         return this.year;
     }
+
+    /**
+     * Returns the month from the date object
+     *
+     * @return integer value representing the month of the date
+     */
 
     public int getMonth(){
         return this.month;
     }
 
+    /**
+     * Returns the day from the date object
+     *
+     * @return integer value representing the day of the date
+     */
+
     public int getDay(){
         return this.day;
     }
 
+    /**
+     * Checks if the date is valid and greater than
+     * 1900 while taking into account of leap years
+     *
+     * @return true if the date is valid and greater than
+     * year 1900, false otherwise
+     */
     public boolean isValid() {
         boolean lessThanCurrDate = !this.greaterThanCurrDate();
         boolean dayValidator = this.dayValidator();
-        boolean publishedAfter1990 =  this.year > OLDBOOK;
+        boolean publishedAfter1900 =  this.year > OLD_BOOK;
 
-        return lessThanCurrDate && dayValidator && publishedAfter1990;
+        return lessThanCurrDate && dayValidator && publishedAfter1900;
     }
 
-    //checks if year is a leap year
+    /**
+     * Checks whether or not the date's year is a leap year
+     *
+     * @return true if the date is in a leap year, false otherwise
+     */
     private boolean isLeapYear(){
         if(this.year % QUADRENNIAL == 0){
             if(this.year % CENTENNIAL == 0){
@@ -68,13 +112,19 @@ public class Date {
         }
     }
 
-    //checks if the mm/dd is valid
+    /**
+     * Checks if the day is valid based off the given month,
+     * taking into account of leap years as well.
+     *
+     * @return true if the day is within the range of the given month,
+     *         false otherwise.
+     */
     private boolean dayValidator(){
         int month = this.month;
         int day = this.day;
         boolean day31 = false;
 
-        if(day>MONTHEND || day<1)
+        if(day>MONTH_END || day<1)
             return false;
 
         switch(month){
@@ -89,11 +139,11 @@ public class Date {
                 break;
 
             case Calendar.FEBRUARY+1:
-                if(this.day>=MONTHEND-1) //in the case of FEB the day cannot be 30 or greater
+                if(this.day >= MONTH_END-1) //in the case of FEB the day cannot be 30 or greater
                     return false;
-                if(this.isLeapYear() && this.day==FEBRUARYLEAP)
+                if(this.isLeapYear() && this.day == FEBRUARY_LEAP)
                     return true;
-                return this.day < FEBRUARYLEAP;
+                return this.day < FEBRUARY_LEAP;
 
             case Calendar.APRIL+1:
             case Calendar.JUNE+1:
@@ -104,10 +154,17 @@ public class Date {
             default: return false;
         }
 
-        return day31 || this.day != MONTHEND;
+        return day31 || this.day != MONTH_END;
     }
 
-    //checks if the date of book is set past the current date
+    /**
+     * Helper method for isValid().
+     * Checks whether the date is ahead of the current Date.
+     *
+     * @return true if the date is not greater than the current date,
+     *         false otherwise.
+     */
+
     private boolean greaterThanCurrDate(){
         Date currDate = new Date();
         if(this.year > currDate.year){
@@ -128,6 +185,12 @@ public class Date {
             }
         }
     }
+
+    /**
+     * Returns a string of the date in the format mm/dd/yyyy
+     *
+     * @return String representing the date object
+     */
 
     public String toString(){
         return this.month + "/" + this.day + "/" + this.year;
