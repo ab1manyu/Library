91d01b4d79e16ac783c8de5a77760a5d7d16e8a1
Ashish Shenoy <61940795+ashenoy20@users.noreply.github.com>
2021-02-08 16:20:02 -0500

    Finalized Docs

---
+++ RunProject1.java
@@ -0,0 +1,14 @@
+
+/**
+ * Driver class for calling the run() method in Kiosk
+ * to start the session for the client.
+ *
+ * @author Abimanyu Ananthu, Ashish Shenoy
+ */
+
+public class RunProject1 {
+    public static void main(String[] args) {
+        Kiosk k = new Kiosk();
+        k.run();
+    }
+}
